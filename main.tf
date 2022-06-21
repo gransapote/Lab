da6bf988b27a295651f91b2c3ab0f81aaeb68408
gransapote <gransapote@gmail.com>
2022-06-21 13:38:30 +0200

    Fix:Configure

--- main.tf
+++ main.tf
@@ -5,29 +5,19 @@ provider "aws" {
   //profile = "default"
 }
 
-data "aws_subnet_ids" "selected" {
-vpc_id = "vpc-01f8e0ace904bdd9e"
-filter {
-name = "tag:Name"
-values = ["alumni"] # insert values here
-}
-}
 
-resource "aws_s3_bucket" "web" {
+//resource "aws_s3_bucket" "web" {
   bucket = "mapfre-gitops-amelgar"
   acl = "public-read"
   website {
     index_document = "index.html"
   }
-  //ami           = var.image_id 
-  //instance_type = "t3.micro"
-  //associate_public_ip_address = true
-  //key_name = "amelgar@mapfre.com"
-  //subnet_id = one (toset (data.aws_subnet_ids.selected.ids)) 
-  //vpc_security_group_ids = ["sg-0916b96e36c8b90a4"]
+//}
 
-  //tags = {
-    //Name = "gransapote"
-    //Owner = "amelgar@mapfre.com"
-  //} 
+resource "aws_s3_bucket_website_configuration" "web" {
+  bucket = "mapfre-gitops-amelgar"
+  //acl = "public-read"
+  website {
+    index_document = "index.html"
+  }
 }
