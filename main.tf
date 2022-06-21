6a8a8f67726a935f9c6620e7d8a9f048ee5165e2
gransapote <gransapote@gmail.com>
2022-06-21 13:18:00 +0200

    Update:Terraform Files

---
+++ main.tf
@@ -0,0 +1,33 @@
+/*   Main execution file, usually it has providers configuration */
+  
+provider "aws" {
+  region  = "eu-west-1"
+  //profile = "default"
+}
+
+data "aws_subnet_ids" "selected" {
+vpc_id = "vpc-01f8e0ace904bdd9e"
+filter {
+name = "tag:Name"
+values = ["alumni"] # insert values here
+}
+}
+
+resource "aws_s3_bucket" "web" {
+  bucket = "mapfre-gitops-amelgar"
+  acl = "public-read"
+  website {
+    index_document = "index.html"
+  }
+  //ami           = var.image_id 
+  //instance_type = "t3.micro"
+  //associate_public_ip_address = true
+  //key_name = "amelgar@mapfre.com"
+  //subnet_id = one (toset (data.aws_subnet_ids.selected.ids)) 
+  //vpc_security_group_ids = ["sg-0916b96e36c8b90a4"]
+
+  //tags = {
+    //Name = "gransapote"
+    //Owner = "amelgar@mapfre.com"
+  //} 
+}
