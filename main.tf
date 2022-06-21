26ea859a3313189e01d3a90cedff96acc403389e
gransapote <gransapote@gmail.com>
2022-06-21 13:53:42 +0200

    Fix:Configure

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 //}
 
 resource "aws_s3_bucket_website_configuration" "web" {
-  bucket = aws_s3_bucket.web.bucket
+  bucket = "aws_s3_bucket.web.bucket"
   //acl = "public-read"
   website {
     index_document = "index.html"
@@ -23,6 +23,6 @@ resource "aws_s3_bucket_website_configuration" "web" {
 }
 
 resource "aws_s3_bucket_acl" "web_bucket_acl" {
-  bucket = aws_s3_bucket.web.id
+  bucket = "aws_s3_bucket.web.id"
   acl    = "public-read"
 }
