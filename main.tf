86981fa5ee7c52000b2c0c6bf7314b4f015cbd77
gransapote <gransapote@gmail.com>
2022-06-21 15:09:59 +0200

    Fix:Configure

--- main.tf
+++ main.tf
@@ -6,23 +6,23 @@ provider "aws" {
 }
 
 
-//resource "aws_s3_bucket" "web" {
-//  bucket = "mapfre-gitops-amelgar"
+resource "aws_s3_bucket" "mapfre-gitops-amelgar" {
+  bucket = "mapfre-gitops-amelgar"
 //  acl = "public-read"
 //  website {
 //    index_document = "index.html"
 //  }
-//}
+}
 
-resource "aws_s3_bucket_website_configuration" "web" {
-  bucket = "aws_s3_bucket.web.bucket"
+resource "aws_s3_bucket_website_configuration" "mapfre-gitops-amelgar" {
+  bucket = "aws_s3_bucket.webmapfre-gitops-amelgar.bucket"
   //acl = "public-read"
   index_document {
     suffix = "index.html"
   }
 }
 
-resource "aws_s3_bucket_acl" "web_bucket_acl" {
-  bucket = "aws_s3_bucket.web.bucket"
+resource "aws_s3_bucket_acl" "mapfre-gitops-amelgar" {
+  bucket = "aws_s3_bucket.mapfre-gitops-amelgar.bucket"
   acl    = "public-read"
 }
