449f2dd4a67dd1522e36334bcaed22b9ea5e87e4
gransapote <gransapote@gmail.com>
2022-06-21 16:15:45 +0200

    Fix:Terraform

--- variables.tf
+++ variables.tf
@@ -2,25 +2,11 @@
   //type = string
 //}
 
-variable "availability_zone_names" {
-  type    = list(string)
-  default = ["eu-west-1a"]
-}
+//variable "availability_zone_names" {
+//  type    = list(string)
+//  default = ["eu-west-1a"]
+//}
 
-variable "docker_ports" {
-  type = list(object({
-    internal = number
-    external = number
-    protocol = string
-  }))
-  default = [
-    {
-      internal = 8300
-      external = 8300
-      protocol = "tcp"
-    }
-  ]
-}
 provider "aws" {
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
