89d61884c0f5846accaa00345535c2543eff5f01
gransapote <gransapote@gmail.com>
2022-06-23 11:11:36 +0200

    Fix: Variables

---
+++ modules/variables.tf
@@ -0,0 +1,11 @@
+variable aws_access_key_id {
+  type        = string
+  default     = "AKIA3IT5FKBPS5NEWQXI"
+  description = "description"
+}
+variable aws_secret_access_key {
+  type        = string
+  default     = "fW2xPPviYvMnrwW8htPiWT2nGpNGZYHDQ4rD/puE"
+  description = "description"
+}
+
---
+++ variables.tf
@@ -0,0 +1,9 @@
+
+variable "aws_access_key_id" {
+     type = string
+     default = "Access key to AWS console"
+}
+variable "aws_secret_access_key" {
+     type = string
+     default = "Secret key to AWS console"
+}
