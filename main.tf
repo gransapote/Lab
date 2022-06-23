104541abf9b3b112fc0980bfca3e5ae7ca56b49b
gransapote <gransapote@gmail.com>
2022-06-23 11:33:01 +0200

    Fix: Creden

--- main.tf
+++ main.tf
@@ -7,8 +7,10 @@ provider "aws" {
   //access_key = "${var.aws_access_key_id}"
   //secret_key = "${var.aws_secret_access_key}"
   //profile = "default"
-  access_key_id = "${var.aws_access_key_id}"
-  secret_key = "${var.aws_secret_access_key}"
+  //access_key_id = "${var.aws_access_key_id}"
+  //secret_key = "${var.aws_secret_access_key}"
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
 
--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 
-variable "aws_access_key_id" {
+variable "access_key" {
      type = string
      default = "Access key to AWS console"
 }
-variable "aws_secret_access_key" {
+variable "secret_key" {
      type = string
      default = "Secret key to AWS console"
 }
